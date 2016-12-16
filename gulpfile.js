73a166f011389296a94c6805e907bd8bb863a4a0
Sam Atkins <sam@cubi.io>
2016-12-16 10:57:29 +0000

    chore: Add build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,10 @@ const uglify = require('gulp-uglify');
 const debug = require('gulp-debug');
 const cached = require('gulp-cached');
 const cssnano = require('gulp-cssnano');
+const responsiveGm = require('gulp-responsive-images');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const surge = require('gulp-surge');
 
 gulp.task('hello', function() {
 	return console.log('Hello World');
@@ -159,3 +163,100 @@ gulp.task('clean:dist', function (callback) {
 	return del.sync(['dist']);
 })
 
+// use GM then move to folder responsive
+gulp.task('imagesgm', function() {
+    return gulp.src('app/images_src/**/*')
+        .pipe(responsiveGm({
+            '*.+(jpg|jpeg|png)': [{
+                width: 1600,
+                suffix: '_1600_large_2x',
+                quality: 75
+            }, {
+                width: 800,
+                suffix: '_800_large_1x',
+                quality: 70
+            }, {
+                width: 600,
+                suffix: '_medium',
+                quality: 60
+            }, {
+                width: 500,
+                height: 375,
+                crop: 'center',
+                suffix: '_small',
+                quality: 50
+            }],
+        }))
+        .pipe(gulp.dest('app/responsive/'));
+});
+
+// use imagemin then move to folder images 
+// correct task - $ and cache ?
+gulp.task('imagesmin', function() {
+    return gulp.src('app/responsive/**/*.+(png|jpg|jpeg|gif|svg)')
+        .pipe(cache(imagemin()))
+        // where is the pipe dest? to app or dist? 
+        .pipe(gulp.dest('app/images/'));
+});
+
+// copies any images in fixed to images folder 
+gulp.task('copy-fixed-images', function() {
+    return gulp.src('./app/images_src/fixed/**/*')
+        .pipe(gulp.dest('./app/images/'));
+});
+
+// cleans responsive folder
+gulp.task('responsive-tidy', function() {
+    return del([
+        './app/responsive/**/*'
+    ]);
+})
+
+// clears cache for imagemin 
+gulp.task('cache:clear', function (callback) {
+	return cache.clearAll(callback)
+})
+
+// combo build task for responsive (gm) and min images
+gulp.task('build-images', function(callback) {
+    runSequence('imagesgm', ['imagesmin', 'copy-fixed-images'],
+        'responsive-tidy',
+        callback);
+});
+
+gulp.task('fonts', function() {
+	return gulp.src('app/fonts')
+		.pipe(gulp.dest('dist'))
+});
+
+gulp.task('clean:dist', function (callback) {
+	return del.sync(['dist']);
+})
+
+gulp.task('build', function() {
+	runSequence(
+		['clean:dev', 'clean:dist'],
+		['sprites', 'lint:js', 'lint:scss'],
+		'sass',
+		['useref', 'build-images', 'fonts', 'test'],
+		callback
+		);
+})
+
+gulp.task('serve:dist', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'dist'
+		},
+		browser: 'google chrome',
+		notify: false		
+	})
+})
+
+// deploy to surge.sh
+gulp.task('surge', [], function () {
+  return surge({
+    project: 'dist',                        // Path to dist directory
+    domain: 'http://add-name.surge.sh/'  // amend url
+  })
+})
