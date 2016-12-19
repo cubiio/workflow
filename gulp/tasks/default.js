96a5254f5c8501de7c69eb30c5855be1a255fc9c
Sam Atkins <sam@cubi.io>
2016-12-19 14:35:37 +0000

    chore: Update build tasks

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -5,8 +5,12 @@ const runSequence = require('run-sequence');
 gulp.task('default', function(callback) {
 	runSequence(
 		'clean:dev', 
-		['sprites', 'lint:js', 'lint:scss'],
-		'sass',
+
+		// toggle if vendor js causes issues:
+		// ['sprites', 'lint:js', 'lint:scss'],
+		['sprites', 'lint:scss'],
+		
+		['sass', 'images'],
 		['browserSync', 'watch'],
 		callback
 		);
